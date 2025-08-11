dd34ff261cdb55d97395bcb600e7dc93efb15f02
4drianC4 <camararequeadrianrafael@gmail.com>
2025-08-11 11:23:10 -0400

    feat: update Title component styles and integrate Noto Sans Telugu font

--- src/components/molecules/Title.tsx
+++ src/components/molecules/Title.tsx
@@ -6,8 +6,8 @@ type Props = {
 export const Title = ({ title, description }: Props) => {
     return (
         <div className="text-center">
-            <h1 className="text-2xl font-bold">{title}</h1>
-            {description && <p className="text-sm text-gray-500 mt-2">{description}</p>}
+            <h1 className="color-textoscuro font-noto-telugu" style={{ fontSize: '48px', fontWeight: 600 }}>{title}</h1>
+            {description && <p className="text-sm color-textoscuro mt-2">{description}</p>}
         </div>
     );
 }
